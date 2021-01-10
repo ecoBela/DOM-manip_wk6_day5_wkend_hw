21536a1ea839ce9e53ddd0bb1c8303d417d98b3a
Bela <belahamid@gmail.com>
2021-01-10 16:41:05 +0000

    add two variables to handleSubmit()

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
 // handleSubmit function should do the following:
 const handleSubmit = (event) => {
     event.preventDefault();
+    const newListItem = document.createElement('li');
+    const list = document.querySelector('ul');
 
 }
 // *take the event parameter
 // * event.preventDefault();
-// 1. create a new li element, store in variable
+// 1. create a new li element, store in variable (done)
 // 1a. select ul, store in variable
 // 2. appendChild new li element to the ul
 // 3. capture value typed in
