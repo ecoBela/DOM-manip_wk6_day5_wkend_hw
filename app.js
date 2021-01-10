b7dd536c3b63a95ac02bb0498e4cf54ee52530e4
Bela <belahamid@gmail.com>
2021-01-10 14:56:32 +0000

    initial commit: added form to index.html

---
+++ app.js
@@ -0,0 +1,17 @@
+// Add event listener to the whole page e.g. ('DOMContentLoaded)
+// Create a variable/object for the form element
+// Add event listener to submit button
+// handleSubmit function should do the following:
+// *take the event parameter
+// * event.preventDefault();
+// 1. create a new li element, store in variable
+// 1a. select ul, store in variable
+// 2. appendChild new li element to the ul
+// 3. capture value typed in
+// 4. append text to li element e.g. .textContent(event.target.value?)
+// 5. reset the form with event.target.reset();
+
+console.log("JS file is connected")
+document.addEventListener('DOMContentLoaded', () => {
+    
+})
